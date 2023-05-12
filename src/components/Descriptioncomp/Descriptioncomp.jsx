e46a3b8b83864386a3155fb83616fd05eb709470
Dk314053 <dk314053@gmail.com>
2023-05-12 12:54:56 +0530

    final changes

--- src/components/Descriptioncomp/Descriptioncomp.jsx
+++ src/components/Descriptioncomp/Descriptioncomp.jsx
@@ -2,7 +2,6 @@ import  React from 'react';
 import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import styles from './Descriptioncomp.module.css'
@@ -10,6 +9,7 @@ import SubtitlesIcon from '@mui/icons-material/Subtitles';
 import Description from './Description';
 import DescriptionActivity from './DescriptionActivity';
 import DescriptionComment from './DescriptionComment';
+import ClearIcon from '@mui/icons-material/Clear';
 
 export default function Descriptioncomp() {
   const [open, setOpen] = useState(false);
@@ -33,10 +33,11 @@ export default function Descriptioncomp() {
       <Dialog
         open={open}
         onClose={handleClose}
-      sx={{backgroundColor:"#091e420a"}}
+      sx={{backgroundColor:"#091e420a",height:"100%"}}
         >
        
         <DialogContent   sx={{backgroundColor:"#091e420a"}} >
+        <Button sx={{marginLeft:"90%",}} onClick={handleClose}><ClearIcon /></Button>
         <span className={styles.title}><SubtitlesIcon /> <input value={update} type='text' onChange={(e)=>setUpdate(e.target.value)} className={styles.input}/> </span>
                        <p className={styles.titlepara}>in list <span style={{textDecoration:"underline",marginLeft:"3px"}}> To Do </span> <VisibilityIcon sx={{fontSize:"medium",marginLeft:"8px"}}/></p> 
             
@@ -45,10 +46,7 @@ export default function Descriptioncomp() {
           <DescriptionComment />
          
         </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-
-        </DialogActions>
+      
       </Dialog>
     </div>
   );

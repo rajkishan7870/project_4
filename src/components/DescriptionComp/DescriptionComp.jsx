03b3bacdea5337b42e071d3aabd7f9e018f88ae7
kishan kumar <rajkishan7870@gmail.com>
2023-05-17 15:21:42 +0530

    kishan day-6 with redux, edit, delete, and go to description

--- src/components/DescriptionComp/DescriptionComp.jsx
+++ src/components/DescriptionComp/DescriptionComp.jsx
@@ -1,56 +1,58 @@
-import  React from 'react';
-import { useState } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import styles from './Descriptioncomp.module.css'
-import SubtitlesIcon from '@mui/icons-material/Subtitles';
-import Description from './Description';
-import DescriptionActivity from './DescriptionActivity';
-import DescriptionComment from './DescriptionComment';
+import React from "react";
+import { useState } from "react";
+import { Button, Card, CardContent, CardActions } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import styles from "./Descriptioncomp.module.css";
+import SubtitlesIcon from "@mui/icons-material/Subtitles";
+import Description from "./Description";
+import DescriptionActivity from "./DescriptionActivity";
+import DescriptionComment from "./DescriptionComment";
+import { Navigate, useParams } from "react-router-dom";
 
 export default function Descriptioncomp() {
-  const [open, setOpen] = useState(false);
- const[update,setUpdate]=useState("Todo Title")
+  const [update, setUpdate] = useState("");
+  const params = useParams();
 
-  const handleClickOpen = (scrollType) => () => {
-    setOpen(true);
-   
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-
-
+  function handleClose() {
+    Navigate("/");
+  }
   return (
     <div>
-      <Button onClick={handleClickOpen()}>scroll=paper</Button>
-     
-      <Dialog
-        open={open}
+      <Card
         onClose={handleClose}
-      sx={{backgroundColor:"#091e420a"}}
-        
+        sx={{
+          backgroundColor: "#091e420a",
+          width: "50%",
+          marginLeft: "20%",
+        }}
       >
-       
-        <DialogContent   sx={{backgroundColor:"#091e420a"}} >
-        <span className={styles.title}><SubtitlesIcon /> <input value={update} type='text' onChange={(e)=>setUpdate(e.target.value)} className={styles.input}/> </span>
-                       <p className={styles.titlepara}>in list <span style={{textDecoration:"underline",marginLeft:"3px"}}> To Do </span> <VisibilityIcon sx={{fontSize:"medium",marginLeft:"8px"}}/></p> 
-            
-           <Description />
-       <DescriptionActivity />
-          <DescriptionComment />
-         
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
+        <CardContent sx={{ backgroundColor: "#091e420a" }}>
+          <span className={styles.title}>
+            <SubtitlesIcon />{" "}
+            <input
+              value={params.id}
+              type="text"
+              onChange={(e) => setUpdate(e.target.value)}
+              className={styles.input}
+            />{" "}
+          </span>
+          <p className={styles.titlepara}>
+            in list{" "}
+            <span style={{ textDecoration: "underline", marginLeft: "3px" }}>
+              {" "}
+              To Do{" "}
+            </span>{" "}
+            <VisibilityIcon sx={{ fontSize: "medium", marginLeft: "8px" }} />
+          </p>
 
-        </DialogActions>
-      </Dialog>
+          <Description />
+          <DescriptionActivity />
+          <DescriptionComment />
+        </CardContent>
+        <CardActions>
+          <Button onClick={handleClose}>Cancel</Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
--- src/components/HomeComp/card.jsx
+++ src/components/HomeComp/card.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import style from './card.module.css'
+import React from "react";
+import style from "./card.module.css";
+import EditIcon from "@mui/icons-material/Edit";
+import ClearIcon from "@mui/icons-material/Clear";
 
-export default function Card({cardInfo}) {
+export default function Card({ cardInfo, handleEdit, handleDelete, onClick }) {
   return (
-    <div className={style.card}>
-        {cardInfo.title}
+    <div onClick={onClick} className={style.card}>
+      {cardInfo.title}
+      {
+        <div className={style.cardDelete}>
+          <EditIcon onClick={handleEdit} />
+          <ClearIcon onClick={handleDelete} />
+        </div>
+      }
     </div>
-  )
+  );
 }

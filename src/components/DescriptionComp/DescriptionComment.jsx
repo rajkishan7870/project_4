be0d82987ca77d3870d6ce35caa9948ae9eb49d1
Dk314053 <dk314053@gmail.com>
2023-05-18 12:52:48 +0530

    time added

--- src/components/DescriptionComp/DescriptionComment.jsx
+++ src/components/DescriptionComp/DescriptionComment.jsx
@@ -17,15 +17,19 @@ function DescriptionComment() {
   
 
   const date = new Date();
-
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
   const showTime =
     date.toLocaleString("default", { month: "long" }) +
     " " +
     date.getDate() +
     " at " +
-    date.getHours() +
-    ":" +
-    date.getMinutes();
+   strTime;
 
     const [time, setTime] = useState(showTime);
 

d2cebab34e34ea5e6434710bbd7cb2dfd84bb2b8
Madhushraba <madhushraba21das@gmail.com>
2023-05-18 08:46:22 +0530

    bg.module.css updated

--- src/redux/slice.js
+++ src/redux/slice.js
@@ -1,56 +1,49 @@
-import { createSlice} from "@reduxjs/toolkit";
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const listSlice = createSlice({
-    name : "listSlice",
-    initialState : {
-        list : [],
-        background : [],
+  name: "listSlice",
+  initialState: {
+    list: [],
+    background: [],
+  },
+  reducers: {
+    addList: (state, action) => {
+      state.list.push(action.payload);
     },
-    reducers : {
-        addList : (state,action) =>{
-            state.list.push(action.payload);
-        },
-        addCard : (state,action) =>{
-            state.list.forEach((item)=>{
-               if(item.id === action.payload.cardid){
-                if(Object.hasOwn(item, "children")){
-                    item.children.push(action.payload)
-                }
-                else{
-                    item.children = [];
-                    item.children.push(action.payload)
-                }
-               } 
-            })
-        },
-        editCard : (state,action) =>{
-            console.log(action.payload)
-            for(let i=0;i<state.list.length;i++){
-                if(state.list[i].id === action.payload.cardid){
-                    state.list[i].children[action.payload.index].title = "kishan"
-                }
-            }
-            
-        },
-        removeCard : (state,action) =>{
-            for(let i=0;i<state.list.length;i++){
-                if(state.list[i].id === action.payload.cardid){
-                    state.list[i].children.splice(action.payload.index,1)
-                }
-            }
-            
-        },
-        changeBackground :(state,action)=>{
-            
-            state.background.push(action.payload)
-        },
-        
-    }
-})
+    addCard: (state, action) => {
+      state.list.forEach((item) => {
+        if (item.id === action.payload.cardid) {
+          if (Object.hasOwn(item, "children")) {
+            item.children.push(action.payload);
+          } else {
+            item.children = [];
+            item.children.push(action.payload);
+          }
+        }
+      });
+    },
+    editCard: (state, action) => {
+      console.log(action.payload);
+      for (let i = 0; i < state.list.length; i++) {
+        if (state.list[i].id === action.payload.cardid) {
+          state.list[i].children[action.payload.index].title = "kishan";
+        }
+      }
+    },
+    removeCard: (state, action) => {
+      for (let i = 0; i < state.list.length; i++) {
+        if (state.list[i].id === action.payload.cardid) {
+          state.list[i].children.splice(action.payload.index, 1);
+        }
+      }
+    },
+    changeBackground: (state, action) => {
+      state.background.push(action.payload);
+    },
+  },
+});
 
+export const { addList, addCard, editCard, removeCard, changeBackground } =
+  listSlice.actions;
 
-
-export const {addList, addCard, editCard, removeCard , changeBackground} = listSlice.actions;
-
-export default listSlice.reducer ;
+export default listSlice.reducer;
